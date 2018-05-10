50afb2c7438f4a824b6cc6c59197b02f78f648d2
Kevin Choubacha <chewbacha@gmail.com>
2018-05-10 15:13:31 -0700

    Remove function creation

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
 macro_rules! test {
     ($type_to_check:ident, $type_i:expr) => {
-        pub fn $type_to_check() {
-            let json = format!(
-                // Will indent 3 times with each cargo fmt call
-                r#"{{
-                    "id":"1",
-                    "paging_token":"7",
-                    "type_i":{},
-                    "type":"{}",
-                    "transaction_hash":"123"
-                }}"#,
-                $type_i,
-                stringify!($type_to_check),
-            );
-            println!("{}", json);
-        }
+        let json = format!(
+            // Will indent 3 times with each cargo fmt call
+            r#"{{
+                "id":"1",
+                "paging_token":"7",
+                "type_i":{},
+                "type":"{}",
+                "transaction_hash":"123"
+            }}"#,
+            $type_i,
+            stringify!($type_to_check),
+        );
+        println!("{}", json);
     };
 }
 
-test!(bar, 10);
-
 fn main() {
+    test!(bar, 10);
+
     // Does not change with cargo fmt
     let test = r#"{{
         "id":"1",
@@ -50,5 +48,4 @@ fn main() {
         }}"#,
         12, "thing"
     );
-    bar();
 }
