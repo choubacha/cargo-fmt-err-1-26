565f996b537943524e7d6ed3f86fa6dc3b49d3a8
Kevin Choubacha <chewbacha@gmail.com>
2018-05-10 15:12:20 -0700

    Remove module

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
-mod foo {
-    macro_rules! test {
-        ($type_to_check:ident, $type_i:expr) => {
-            pub fn $type_to_check() {
-                let json = format!(
-                    // Will indent 3 times with each cargo fmt call
-                    r#"{{
-                        "id":"1",
-                        "paging_token":"7",
-                        "type_i":{},
-                        "type":"{}",
-                        "transaction_hash":"123"
-                    }}"#,
-                    $type_i,
-                    stringify!($type_to_check),
-                );
-                println!("{}", json);
-            }
-        };
-    }
-
-    test!(bar, 10);
+macro_rules! test {
+    ($type_to_check:ident, $type_i:expr) => {
+        pub fn $type_to_check() {
+            let json = format!(
+                // Will indent 3 times with each cargo fmt call
+                r#"{{
+                    "id":"1",
+                    "paging_token":"7",
+                    "type_i":{},
+                    "type":"{}",
+                    "transaction_hash":"123"
+                }}"#,
+                $type_i,
+                stringify!($type_to_check),
+            );
+            println!("{}", json);
+        }
+    };
 }
+
+test!(bar, 10);
 
 fn main() {
     // Does not change with cargo fmt
@@ -52,5 +50,5 @@ fn main() {
         }}"#,
         12, "thing"
     );
-    foo::bar();
+    bar();
 }
